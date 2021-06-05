92f208e949f77874a0207d842187652d2b485818
yoridori <midorika809@gmail.com>
2021-06-05 14:12:35 +0900

    重複無し、Max52枚まで取得。削除処理実装。

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,120 @@
 const BRACK_JACK: number = 21;
 let myCardTotal: number = 0;
-let myDispCards: CardImpl[] = [];
 let myCalcCards: number[] = [];
+let myDispCardInfo: CardInfo[] = [];
+
+let clubs: number[] = [];
+let diamonds: number[] = [];
+let hearts: number[] = [];
+let spades: number[] = [];
+
+type CardSymbol = "♣" | "♦" | "❤" | "♠";
+
+interface CardAnother {
+  cardNum: number;
+  cardSymbol: CardSymbol;
+  cardColor: string;
+  equal(card: CardAnother): boolean;
+}
+class CardInfo implements CardAnother {
+  cardNum: number;
+  cardSymbol: CardSymbol;
+  cardColor: string;
+
+  constructor(cardNum: number, cardSymbol: CardSymbol, cardColor: string) {
+    this.cardNum = cardNum;
+    this.cardSymbol = cardSymbol;
+    this.cardColor = cardColor;
+  }
+
+  equal(cardInfo: CardAnother) {
+    if (
+      this.cardNum === cardInfo.cardNum &&
+      this.cardSymbol === cardInfo.cardSymbol &&
+      this.cardColor === this.cardColor
+    ) {
+      return true;
+    }
+    return false;
+  }
+}
+
+/*
+ * 初回読み込み処理
+ */
+function onLoad(): void {
+  init();
+}
+
+/*
+ * 初期化処理
+ */
+function init(): void {
+  clubs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  diamonds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  hearts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  spades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  myCardTotal = 0;
+  myCalcCards = [];
+  myDispCardInfo = [];
+}
+
+/*
+ * カード作成処理
+ */
+function createCard(): CardInfo {
+  let cardSymbol = "" as CardSymbol;
+  let color = "";
+  let popCard: number | undefined = undefined;
+  let canDrow = true;
+  do {
+    const num = randomNumber(0, 3);
+    switch (num) {
+      case CardSymbol.Clubs:
+        cardSymbol = "♣";
+        color = SymbolColor.Black;
+        popCard = clubs
+          .splice(Math.floor(Math.random() * clubs.length), 1)
+          .pop();
+        break;
+      case CardSymbol.Spades:
+        cardSymbol = "♠";
+        color = SymbolColor.Black;
+        popCard = spades
+          .splice(Math.floor(Math.random() * spades.length), 1)
+          .pop();
+        break;
+      case CardSymbol.Diamonds:
+        cardSymbol = "♦";
+        color = SymbolColor.Red;
+        popCard = diamonds
+          .splice(Math.floor(Math.random() * diamonds.length), 1)
+          .pop();
+        break;
+      case CardSymbol.Hearts:
+        cardSymbol = "❤";
+        color = SymbolColor.Red;
+        popCard = hearts
+          .splice(Math.floor(Math.random() * hearts.length), 1)
+          .pop();
+        break;
+      default:
+        color = SymbolColor.None;
+        break;
+    }
+  } while (!popCard);
+  return new CardInfo(popCard, cardSymbol, color);
+}
 
 /*
  * カード追加処理
  */
 function addCard(): void {
-  /* let card = createCard();
-  while(myDispCards.some(dispCard => dispCard.equal(card))){
-    card = createCard();
-  }
-  myDispCards.push(createCard());
-  let cardNumber = myDispCards[myDispCards.length - 1].num;
-  cardNumber = convertOverEleven(cardNumber);
-  myCalcCards.push(cardNumber);
+  let card = createCard();
+  myDispCardInfo.push(card);
+  let cardNum = card.cardNum;
+  cardNum = convertOverEleven(cardNum);
+  myCalcCards.push(cardNum);
   myCalcCards.sort((num1, num2) => num2 - num1);
   myCardTotal = 0;
   for (let num of myCalcCards) {
@@ -23,7 +123,7 @@ function addCard(): void {
     } else {
       myCardTotal += num;
     }
-  } */
+  }
 }
 
 /*
@@ -36,34 +136,6 @@ function randomNumber(min: number, max: number): number {
   return randomInt;
 }
 
-/*
- * カードインタフェース
- */
-interface Card {
-  num: number;
-  cardSymbol: string;
-  color: string;
-  equal(card:Card): boolean;
-}
-
-class CardImpl implements Card {
-  num: number;
-  cardSymbol: string;
-  color: string;
-
-  constructor(num: number, cardSymbol: string, color: string){
-      this.num = num;
-      this.cardSymbol = cardSymbol;
-      this.color = color;
-  }
-
-  equal(card: Card): boolean{
-      if(this.num === card.num && this.cardSymbol === card.cardSymbol && this.color === card.color){
-          return true;
-      }
-      return false;
-  }
-}
 /*
  * カード記号を列挙
  */
@@ -84,44 +156,6 @@ const SymbolColor = {
   None: "",
 } as const;
 
-function createCard(): Card {
-  const num = getCardNumber();
-  const symbolNum = randomNumber(0, 3);
-  const cardSymbol = getCardSymbol(symbolNum);
-  const color = getCardColor(symbolNum);
-  return new CardImpl(num, cardSymbol, color);
-}
-
-function getCardNumber(): number {
-  let cardNumber = randomNumber(1, 13);
-  return cardNumber;
-}
-
-/*
- * ランダムな記号を決定する
- */
-function getCardSymbol(num: number): string {
-  let cardSymbol = CardSymbolDisp[num];
-  return cardSymbol;
-}
-
-function getCardColor(num: number): string {
-  let color = "";
-  switch (num) {
-    case CardSymbol.Clubs:
-    case CardSymbol.Spades:
-      color = SymbolColor.Black;
-      break;
-    case CardSymbol.Diamonds:
-    case CardSymbol.Hearts:
-      color = SymbolColor.Red;
-      break;
-    default:
-      color = SymbolColor.None;
-      break;
-  }
-  return color;
-}
 /*
  * 11〜13は10としてカウントする
  */
@@ -149,15 +183,12 @@ function convertOneOrEleven(num: number): number {
 /*
  * HTMLにカードの追加
  */
-function addElement(cards: Card[]): void {
+function addElement(cards: CardAnother[]): void {
   let cardHtml = `<div id="cards"><label>`;
   cards.forEach(
     (card, index) =>
-      (cardHtml += `[${index + 1}: ${card.cardSymbol}${card.num}]`)
+      (cardHtml += `[${index + 1}: ${card.cardSymbol}${card.cardNum}]`)
   );
-  // for (let card of cards) {
-  //   cardHtml += `[1: ${card.cardSymbol}${card.num}]`;
-  // }
   $("#just-cards").append(cardHtml);
 }
 
@@ -169,13 +200,13 @@ function deleteElement(): void {
 }
 
 /*
- * 21になったらWinnerメッセージを出す
+ * Winnerメッセージを返す
  */
 function getWinnerMessage(): string {
   return "WIN!!!";
 }
 /*
- * 22以上になったらLoserメッセージを出す
+ * Loserメッセージを返す
  */
 function getLoserMessage(): string {
   return "Lose...";
@@ -187,9 +218,25 @@ function getLoserMessage(): string {
 $("#just-drow").on("click", function () {
   addCard();
   deleteElement();
-  addElement(myDispCards);
+  addElement(myDispCardInfo);
+  if (
+    clubs.length === 0 &&
+    spades.length === 0 &&
+    diamonds.length === 0 &&
+    hearts.length === 0
+  ) {
+    $(this).prop("disabled", true);
+  }
 });
 
 /*
  * 最初から始めるボタン処理
  */
+$("#just-clear").on("click", function () {
+  deleteElement();
+  init();
+  $("#just-drow").prop("disabled", false);
+});
+
+// 初期化処理
+onLoad();
